75b5341a9b5e152436e8e8fa2db881c1a54b4fea
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2024-02-24 10:50:43 +0600

    Cors setup

--- src/server.js
+++ src/server.js
@@ -1,16 +1,7 @@
 const app = require("./app");
 require("dotenv").config();
-// const port = process.env.PORT || 3001;
-
-// app.get('/', (req, res) => {
-//     res.send('Call Center Backend is Runinng!')
-// })
-
-// app.listen(port, () => {
-//     console.log(`Call Center is listening on port ${port}`)
-// })
 
 const port = process.env.PORT || 3000;
-app.listen(port, "192.168.10.3", () => {
-  console.log(`Call Center Server is listening on 45.55.201.7:${port}`);
+app.listen(port, "192.168.10.14", () => {
+  console.log(`Call Center Server is listening on 192.168.10.14:${port}`);
 });
