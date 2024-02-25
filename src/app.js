4e5f5b6b7d4b875bf22447f1d4a5b1efe461b203
opuopu <nurmdopu428@gmail.com>
2024-02-25 12:08:34 +0600

    refactor:change ip

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "165.227.211.138",
+    origin: "http://localhost:3001",
     //[
     //   process.env.ALLOWED_CLIENT_URL_DASHBOARD,
     //   process.env.ALLOWED_CLIENT_URL_WEB,
