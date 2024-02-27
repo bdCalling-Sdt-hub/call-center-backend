1428d0a62caba2e940674c93bd92c61f29bca138
opuopu <nurmdopu428@gmail.com>
2024-02-27 16:55:29 +0600

    feat: added successfully

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.connect(process.env.MONGODB_CONNECTION, {});
 const port = process.env.PORT || 3000;
-app.listen(port, "", () => {
+const url = process.env.SERVER_URL || "";
+app.listen(port, url, () => {
   console.log(`Call Center Server is listening on ${port}`);
 });
